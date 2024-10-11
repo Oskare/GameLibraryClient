66fc0c5494cd2239d9a71d893319c4dd64a6532b
Oskar Eriksson <oskare90@gmail.com>
2024-10-11 21:05:59 +0200

    Added button component

---
+++ src/ui/button/button.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {NgClass} from '@angular/common';
+import {LucideAngularModule} from 'lucide-angular';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+  imports: [
+    NgClass,
+    LucideAngularModule
+  ],
+  standalone: true
+})
+export class ButtonComponent {
+  @Input() label: string = 'Button';
+  @Input() color: string = 'bg-[#0f172ae6] hover:bg-[#0f172ae6]/90';
+  @Input() size: string = 'py-2 px-8 h-10';     // Default size
+  @Input() disabled: boolean = false;      // Disabled state
+
+  @Output() onClick: EventEmitter<void> = new EventEmitter();
+
+  handleClick() {
+    if (!this.disabled) {
+      this.onClick.emit();
+    }
+  }
+}
