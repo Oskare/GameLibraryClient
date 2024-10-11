9e7942fc67b610ac7f45bb8a7ac0e0ebbf4921ae
Oskar Eriksson <oskare90@gmail.com>
2024-10-11 21:26:48 +0200

    login styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {ButtonComponent} from '../../ui/button/button.component';
+import {Library, LucideAngularModule} from 'lucide-angular';
+import {LabelComponent} from '../../ui/label/label.component';
+import {InputComponent} from '../../ui/input/input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgOptimizedImage,
+    ButtonComponent,
+    LucideAngularModule,
+    LabelComponent,
+    InputComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  errorMessage: string = "";
+  protected readonly console = console;
+  protected readonly Library = Library;
 }
