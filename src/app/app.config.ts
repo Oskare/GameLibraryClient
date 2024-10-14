dde6a1e95be45fd34a368d460aacb14ff21d011f
Oskar Eriksson <oskare90@gmail.com>
2024-10-14 19:49:30 +0200

    refactored interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
-import {AuthInterceptor} from './authInterceptor';
+import {AuthInterceptor} from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
