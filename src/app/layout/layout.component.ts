476f634bf5b153e71480db37dff0d2af588db791
Oskar Eriksson <oskare90@gmail.com>
2024-10-14 19:47:47 +0200

    mobile menu

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {ProfileDropdownComponent} from '../profile-dropdown/profile-dropdown.component';
+import {ProfileDropdownComponent} from './profile-dropdown/profile-dropdown.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-layout',
@@ -9,11 +10,29 @@ import {ProfileDropdownComponent} from '../profile-dropdown/profile-dropdown.com
     RouterOutlet,
     RouterLinkActive,
     RouterLink,
-    ProfileDropdownComponent
+    ProfileDropdownComponent,
+    NgClass
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 
+  mobileMenuOpen: boolean = false;
+
+  setMobileMenuOpen(open: boolean): void {
+    this.mobileMenuOpen = open;
+  }
+
+  menuClick(): void {
+    this.mobileMenuOpen = false;
+  }
+
+  // Close the dropdown if clicked outside
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!event.target.closest('#mobile-menu') && !event.target.closest('#mobile-menu-btn')) {
+      this.mobileMenuOpen = false;
+    }
+  }
 }
